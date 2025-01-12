9fdd38dbe6908e18f4e666d15d9dad4d8de6a45e
Brijesh Pandey <brijeshpandey18@gmail.com>
2025-01-12 22:41:31 +0530

    hooks directory added

---
+++ sass-assignment/src/hooks/usePagination.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const usePagination = ({ data, itemsPerPage = 5 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(itemsPerPage);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentData = data.slice(startIndex, endIndex);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    setPageSize(itemsPerPage);
+
+  };
+
+
+  return {
+    currentPage,
+    totalPages,
+    currentData,
+    setCurrentPage,
+    handlePageChange,
+  };
+};
