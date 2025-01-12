dabed71257f6c270df933a133126c15fc0e1a406
Brijesh Pandey <brijeshpandey18@gmail.com>
2025-01-12 23:03:43 +0530

    Removed extra logic

--- sass-assignment/src/hooks/usePagination.jsx
+++ sass-assignment/src/hooks/usePagination.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 export const usePagination = ({ data, itemsPerPage = 5 }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(itemsPerPage);
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -11,7 +10,6 @@ export const usePagination = ({ data, itemsPerPage = 5 }) => {
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
-    setPageSize(itemsPerPage);
   };
 
   return {
